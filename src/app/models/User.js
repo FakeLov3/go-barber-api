cf0329656ee946eab132dba068323a9471cbc045
gussouzauni <gustavo10ssouza@gmail.com>
2020-04-19 18:48:09 -0300

    add associate in mongodb

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,6 +26,11 @@ class User extends Model {
         return this;
     }
 
+    static associate(models) {
+        // pertence á
+        this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    }
+
     checkPassword(password) {
         return bcrypt.compare(password, this.password_hash);
     }
